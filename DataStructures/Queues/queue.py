da8870adf15ba6689bdfcb2bbb1242058bc54933
Sriram <sriramreddyp123@gmail.com>
2022-09-24 12:46:29 +0530

    Learned about tuples

--- DataStructures/Queues/queue.py
+++ DataStructures/Queues/queue.py
@@ -1,8 +1,8 @@
-from collections import deque
+# from collections import deque
 
-queue = deque([])
-queue.append(1)
-queue.append(2)
-queue.append(3)
-queue.popleft()
-print(queue)
+# queue = deque([])
+# queue.append(1)
+# queue.append(2)
+# queue.append(3)
+# queue.popleft()
+# print(queue)
---
+++ DataStructures/tuples/tuple.py
@@ -0,0 +1,17 @@
+# read only List
+
+point = (1, 2)
+print(type(point))
+
+point_another = 1,
+print(type(point))
+
+add = point + point_another
+
+
+mul = (1, 2) * 3
+
+list = tuple([1, 2, 3])  # We can pass any iterable
+
+
+# tuples are immutable
