543f919d7f0f7dd48a6113915de98bcf7e45f25c
Sriram <sriramreddyp123@gmail.com>
2022-09-26 11:56:24 +0530

    Learned about properties

---
+++ Classes/Properties.py
@@ -0,0 +1,22 @@
+class Product:
+
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Value canot be negative")
+
+        self.__price = value
+
+    # price = property(get_price, set_price)
+
+
+product = Product(10)
+product.price = -1
+print(product.price)
