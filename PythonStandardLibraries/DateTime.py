cceb2a5caa0b5c6105d486561f443dc2faafb39f
Sriram <sriramreddyp123@gmail.com>
2022-10-05 20:49:34 +0530

    Learned about datetime Objects

---
+++ PythonStandardLibraries/DateTime.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import time
+
+dt = datetime(2018, 1, 1)
+dt = datetime.now()
+datetime.strptime("2018/01/01", "%Y/%m/%d")
+dt = datetime.fromtimestamp(time.time())
+print(dt)
+
+print(f"{dt.year}/{dt.month}")
+print(dt.strftime("%Y/%m"
+                  ))
