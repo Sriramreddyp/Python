725dfe6fd000f695e759fcef6ba33856d364bc0c
Sriram <sriramreddyp123@gmail.com>
2022-10-13 15:14:48 +0530

    Final Commmit in Standard Library Branch

--- PythonStandardLibraries/CSV.py
+++ PythonStandardLibraries/CSV.py
@@ -1,16 +1,16 @@
 import csv
 
-# with open("data.csv", "w") as file:
-#     # Writer = csv.writer(file)  # have to open it as file object
-#     # Writer.writerow(["Trasaction_id"])
-#     # Writer.writerow(["100"])
+with open("data.csv", "w") as file:
+    Writer = csv.writer(file)  # have to open it as file object
+    Writer.writerow(["Trasaction_id"])
+    Writer.writerow(["100"])
 
-# with open("data.csv") as file:
-#     # Reader = csv.reader(file)  # have to open it as file object
-#     # Converting it to a list changes reader's starting postion so it cant interate
-#     # # print(list(Reader))
-#     # for row in Reader:
-#     #     print(row)
+with open("data.csv") as file:
+    Reader = csv.reader(file)  # have to open it as file object
+    # Converting it to a list changes reader's starting postion so it cant interate
+    print(list(Reader))
+    for row in Reader:
+        print(row)
 
-#     # Main Concept - Manipulation
-#     # Python return an object all the time - iterable ,non-iterable
+    # Main Concept - Manipulation
+    # Python return an object all the time - iterable ,non-iterable
